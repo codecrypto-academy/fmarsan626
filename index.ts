6685f216164565bf4e446dede5446a043afeb4e1
Marcos <franmarcos91@gmail.com>
2024-09-21 09:20:12 +0200

    D4

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("hi")
+import { Pool } from "pg";
+
+type CustomerType = {
+    customer_id: string,
+    company_name: string,
+    contact_name: string,
+    contact_title: string,
+    address: string,
+    city: string,
+    region: string,
+    postal_code: string,
+    country: string,
+    phone: string,
+    fax: string
+}
+
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    password: 'pwd',
+});
+
+pool.query("select * from customers limit 2", (err: any, res: any) => {
+    console.log(err, res.rows);
+    pool.end();
+})
+// 
