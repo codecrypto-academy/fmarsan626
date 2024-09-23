7338ce63a82df87816bff63e42d6fd5fe915af33
Marcos <franmarcos91@gmail.com>
2024-09-23 20:57:31 +0200

    D1

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.get("/", (rew, res)=>{
+app.get("/", (req, res)=>{
     res.send("hi")
+})
+
+app.post("/echoPost",(req,res)=>{
+    res.send({body:req.body, qs:req.query})
+})
+app.post("/echoParamPost/:cliente/facturas/:factura",(req,res)=>{
+    res.send({
+        body:req.body,
+        query: req.query,
+        params: req.params
+    })
+})
+app.post("/echoPostJson",(req,res)=>{
+    res.send({bodyJson:req.body})
+})
+app.post("/addUser", (req, res)=>{
+    res.send("Añadido").status(200)
 })
 app.listen(3344)
