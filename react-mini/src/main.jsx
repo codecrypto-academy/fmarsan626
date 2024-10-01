053bfaebabf162fb22b4d16e0577bcd9798bf322
Marcos <franmarcos91@gmail.com>
2024-10-01 21:08:17 +0200

    Day2

---
+++ react-mini/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './componentes/Home'
+import { Productos } from './componentes/Productos'
+import { QueryClient, QueryClientProvider } from 'react-query'
+//import './index.css'
+
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home></Home>}>
+            <Route path='/productos' element={<Productos />} />
+            <Route path='/balance' element={<h1>Balance</h1>} />
+          </Route>
+
+
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>,
+)
