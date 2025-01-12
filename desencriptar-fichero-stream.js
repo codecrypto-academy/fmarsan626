3925bf8d6f0106a8082a7726ddb4425267b93e0e
Marcos <franmarcos91@gmail.com>
2025-01-12 17:46:47 +0100

    Day3

---
+++ desencriptar-fichero-stream.js
@@ -0,0 +1,29 @@
+const { createDecipheriv, createECDH } = require("crypto");
+const { exit, argv } = require("process");
+const args = require("yargs").argv;
+const fs = require("fs");
+
+if (!args.private && !args.public && !args.data) {
+  console.log("Faltan argumentos");
+  exit(0);
+}
+
+const origen = createECDH("secp521r1");
+const key = fs.readFileSync("./data/" + args.private + ".key").toString();
+origen.setPrivateKey(key, "hex");
+
+const pub = fs.readFileSync("./data/" + args.public + ".pb").toString();
+
+// creacion de la clave compartida
+const secret = Uint8Array.from(origen.computeSecret(pub, "hex", "hex"));
+
+// descifrado del fichero
+
+const algo = "aes-256-cbc";
+var descifrador = createDecipheriv(algo, secret.slice(0, 32), secret.slice(0, 16));
+const inputFile = "./data/" + args.private + "-" + args.data + ".enc";
+const outputFile = "./data/" + args.private + "-" + args.data + ".des";
+
+fs.createReadStream(inputFile)
+  .pipe(descifrador)
+  .pipe(new fs.createWriteStream(outputFile))
---
+++ desencriptar-fichero.js
@@ -0,0 +1,33 @@
+const { createDecipheriv, createECDH } = require("crypto");
+const { exit, argv } = require("process");
+const args = require("yargs").argv;
+const fs = require("fs");
+
+if (!args.private && !args.public && !args.data) {
+  console.log("Faltan argumentos");
+  exit(0);
+}
+
+const origen = createECDH("secp521r1");
+const key = fs.readFileSync("./data/" + args.private + ".key").toString();
+origen.setPrivateKey(key, "hex");
+
+const pub = fs.readFileSync("./data/" + args.public + ".pb").toString();
+
+// creacion de la clave compartida
+const secret = Uint8Array.from(origen.computeSecret(pub, "hex", "hex"));
+
+// descifrado del fichero
+
+const algo = "aes-256-cbc";
+var descifrador = createDecipheriv(algo, secret.slice(0, 32), secret.slice(0, 16));
+const inputFile = "./data/" + args.private + "-" + args.data + ".enc";
+console.log(inputFile);
+const texto = fs.readFileSync(inputFile).toString();
+
+let desencriptado = descifrador.update(texto, 'hex', 'utf-8');
+desencriptado += descifrador.final("utf-8");
+
+console.log(desencriptado);
+const outputFile = "./data/" + args.private + "-" + args.data + ".des";
+fs.writeFileSync(outputFile, desencriptado);
